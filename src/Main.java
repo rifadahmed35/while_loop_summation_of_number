484382e47073e4c6abfaa50b5db61d29d8bc40c1
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-05 01:17:17 +0100

    summation of number from 5 to 100.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=5, sum=0;
+        while(i<=100){
+            {sum=sum+i;}
+            i=i+5;
+        }
+        {
+            System.out.println(sum);
+        }
+    }
+}
